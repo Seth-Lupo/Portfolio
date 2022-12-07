1b94d6a7e4cacbbbb022e6102e0a9d108335e0fc
Mundoamundo <57644413+Seth-Lupo@users.noreply.github.com>
2022-12-07 10:32:49 -0500

    tried pixi it sucks

--- client/src/about/Jumbotron.svelte
+++ client/src/about/Jumbotron.svelte
@@ -1,139 +1,113 @@
 <script>
+// @ts-nocheck
+
+    import * as PIXI from 'pixi.js'
+    import Victor from 'victor'
+    import { onMount } from 'svelte'
+  import App from '../App.svelte';
+
+    let app = new PIXI.Application({ 
+        width: window.innerWidth, 
+        height: window.innerHeight/1.61
+    });
+    app.renderer.backgroundColor = 0xFFFFFF;
+
+    let wrapper
+    onMount(() => {wrapper.appendChild(app.view)});
     
-    import P5 from 'p5-svelte';
+    const container = new PIXI.ParticleContainer(40000); 
+    app.stage.addChild(container);
+
+    container.interactive = true
 
 
-    
+    let template = new PIXI.Graphics();
+    template.beginFill(0x000000);
+    template.drawCircle(0, 0, 100)
+    var texture = app.renderer.generateTexture(template);
 
-	const sketch = (p) => {
-
-        p.disableFriendlyErrors = true;
-
-        var particles = []
-
-        class Particle {
-    
-            constructor(posX, posY, velX, velY) {
-                this.pos = p.createVector(posX, posY)
-                this.vel = p.createVector(velX, velY)
-                this.size = 0
-            }
-
-            draw() {
-                
-                // p.strokeWeight(1+this.vel.mag())
-                // p.stroke(1+this.vel.mag()*10, 0, 0)
-                p.vertex(this.pos.x, this.pos.y)
-                
-            }
-
-            advance() {
-                
-                let repulsion = p.createVector(
-                    this.pos.x - p.mouseX + p.width/2, 
-                    this.pos.y - p.mouseY + p.height/2,
-                )
-                repulsion.setMag(Math.min(1/repulsion.magSq(), 0.01))
-                repulsion.mult(2000)
-
-                let inXBounds = 0 < p.mouseX && p.mouseX < p.width
-                let inYBounds = 0 < p.mouseY && p.mouseY < p.height
-
-                if (inXBounds && inYBounds)
-                    this.vel.add(repulsion)
-
-                
-                // let vectorField = p.createVector(
-                //     Math.pow(-this.pos.y, 3) - 1000*this.pos.x,
-                //     Math.pow(this.pos.x, 3) - 1000*this.pos.y
-                // ) 
-
-                // for (let particle of particles) {
-                //     if (this.pos.d)
-                // }
-
-
-                // vectorField.mult(0.00000001)
-                // this.vel.add(vectorField)
-
-
-
-                this.vel.mult(0.8)
-
-
-
-                this.pos.add(this.vel)
-            }
-
+    let particles = new Array(40000).fill(null).map(() => {
+        let particle = new PIXI.Sprite(texture);
+        let data = {
+            pos: Victor(
+                app.renderer.width * Math.random(),
+                app.renderer.height * Math.random()
+            ),
+            vel: Victor(
+                Math.random()-0.5,
+                Math.random()-0.5
+            )
         }
-
-        
-        
-        p.settings = () => {
-             
-            
-
-        }
-
-        p.setup = () => {
-            
-            p.pixelDensity(1);
-            p.frameRate(30)            
-			p.createCanvas(window.screen.width, window.screen.height/1.61, p.WEBGL);
-            
-            for (let i = 0; i < 2000; i++) {
-                particles.push(new Particle(
-                    (Math.random()-0.5) * p.width/2,
-                    (Math.random()-0.5) * p.height/2,
-                    50*(Math.random()-0.5),
-                    50*(Math.random()-0.5)
-                ))
-            }
-                
-
-		};
-
-		p.draw = () => {
-            
-            p.background(255, 255, 255)
-            
-            p.translate(p.width/2, p.height/2)
-
-            for (let particle of particles) {
-                particle.advance()
-            }
-            for (let particle of particles) {
-                p.beginShape(p.POINTS)
-                particle.draw()
-                p.endShape()
-            }
-
-        }
-
-        
-
+        particle.width = 5
+        particle.height = 5
+        particle["data"] = data
+        container.addChild(particle)
+        return particle
+    }) 
     
-
+    let elapsed = 0.0;
+    
+    app.ticker.add((delta) => {
+        elapsed += delta;
         
-      
-	};
+        let mousePos = Victor(
+            app.renderer.plugins.interaction.mouse.global.x,
+            app.renderer.plugins.interaction.mouse.global.y
+        )
+        let inX = mousePos.x > 0 && mousePos.x < app.renderer.width
+        let inY = mousePos.y > 0 && mousePos.y < app.renderer.height
+       
+        
+        particles.map((particle) => {     
+                   
+            if (inX && inY) {
+                let interaction = particle.data.pos.clone()
+                .subtract(mousePos)
+                let factor = 10/interaction.lengthSq()
+                interaction.multiply(Victor(factor, factor))
+                particle.data.vel.add(interaction)
+            }
+
+            let center = Victor(
+                app.renderer.width/2,
+                app.renderer.height/2
+            )
+            let relPos = particle.data.pos.clone().subtract(center)
+            let acc = Victor(
+                0,
+                0
+            ).add(Victor(
+                0.1*relPos.x,
+                0.1*relPos.y
+            ))
+            .add(Victor(
+                600*Math.sin(elapsed/100)*Math.sin((relPos.x)/100),
+                0,
+            ))
+            .multiply(Victor(0.001, 0.001))
+            
+            particle.data.vel = acc
+            
+            particle.data.vel.add(acc)
+
+
+
+            // particle.data.vel.multiply(Victor(0.85, 0.85))
+            particle.data.pos.add(particle.data.vel)
+            
+            
+            particle.x = particle.data.pos.x
+            particle.y = particle.data.pos.y
+
+          
+        })
+
+    });
+
 
 </script>
 
-<main>
-    <P5 {sketch}></P5>
-</main>
+<div bind:this={wrapper}>
 
+</div>
 
-<style>
-
-    main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        /* background-color: aliceblue; */
-        padding: 0;
-        border: 0;
-    }
-
-</style>
