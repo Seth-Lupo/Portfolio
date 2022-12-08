d3b0f2c5139aa173b1c3842cb2ef08e5b446e3b2
Mundoamundo <57644413+Seth-Lupo@users.noreply.github.com>
2022-12-08 13:35:44 -0500

    working jumbotron multicolors slow

--- client/src/about/Jumbotron.svelte
+++ client/src/about/Jumbotron.svelte
@@ -1,113 +1,184 @@
-<script>
-// @ts-nocheck
 
+
+<script>
+    // @ts-nocheck
+    
     import * as PIXI from 'pixi.js'
     import Victor from 'victor'
     import { onMount } from 'svelte'
-  import App from '../App.svelte';
-
-    let app = new PIXI.Application({ 
-        width: window.innerWidth, 
-        height: window.innerHeight/1.61
-    });
-    app.renderer.backgroundColor = 0xFFFFFF;
-
-    let wrapper
-    onMount(() => {wrapper.appendChild(app.view)});
+    import App from '../App.svelte';
     
-    const container = new PIXI.ParticleContainer(40000); 
-    app.stage.addChild(container);
+    let wrapper
+    
+    onMount( () => {
+        let app = new PIXI.Application({ 
+            resizeTo: wrapper,
+            antialiasing: false,
+            transparent: true
 
-    container.interactive = true
-
-
-    let template = new PIXI.Graphics();
-    template.beginFill(0x000000);
-    template.drawCircle(0, 0, 100)
-    var texture = app.renderer.generateTexture(template);
-
-    let particles = new Array(40000).fill(null).map(() => {
-        let particle = new PIXI.Sprite(texture);
-        let data = {
-            pos: Victor(
+        });
+        
+        
+        
+        
+        
+        wrapper.appendChild(app.view)
+        
+        const container = new PIXI.Container(); 
+        app.stage.addChild(container);
+        
+        container.interactive = true
+        
+        let colors = [0x1c1026, 0xc6bbb9, 0x4c1e3c, 0x21242b, 0x7a748c]
+        
+        let textures = []
+        
+        let template1 = new PIXI.Graphics();
+        template1.beginFill(0x4c1e3c);
+        template1.drawCircle(0, 0, 3)
+        let texture1 = app.renderer.generateTexture(template1);
+        textures.push(texture1)
+        
+        let template2 = new PIXI.Graphics();
+        template2.beginFill(0x1c1026);
+        template2.drawCircle(0, 0, 3)
+        let texture2 = app.renderer.generateTexture(template2);
+        textures.push(texture2)
+        
+        
+        let particles = new Array(15000).fill(null).map(() => {
+            
+            let particle = new PIXI.Sprite(textures[Math.floor(Math.random() * textures.length)]);
+            particle.anchor.set(0.5)
+            let data = {
+                pos: Victor(
                 app.renderer.width * Math.random(),
                 app.renderer.height * Math.random()
-            ),
-            vel: Victor(
-                Math.random()-0.5,
-                Math.random()-0.5
-            )
-        }
-        particle.width = 5
-        particle.height = 5
-        particle["data"] = data
-        container.addChild(particle)
-        return particle
-    }) 
-    
-    let elapsed = 0.0;
-    
-    app.ticker.add((delta) => {
-        elapsed += delta;
+                ),
+                vel: Victor(
+                2*(Math.random()-0.5),
+                2*(Math.random()-0.5)
+                )
+            }
+            let size = Math.random() * 10 + 5
+            particle.width = size
+            particle.height = size
+            particle["data"] = data
+            container.addChild(particle)
+            return particle
+        }) 
         
-        let mousePos = Victor(
+        let elapsed = 0.0;
+        
+        app.ticker.maxFPS = 
+        app.ticker.add((delta) => {
+            elapsed += delta;
+            
+            let mousePos = Victor(
             app.renderer.plugins.interaction.mouse.global.x,
             app.renderer.plugins.interaction.mouse.global.y
-        )
-        let inX = mousePos.x > 0 && mousePos.x < app.renderer.width
-        let inY = mousePos.y > 0 && mousePos.y < app.renderer.height
-       
-        
-        particles.map((particle) => {     
-                   
-            if (inX && inY) {
-                let interaction = particle.data.pos.clone()
-                .subtract(mousePos)
-                let factor = 10/interaction.lengthSq()
-                interaction.multiply(Victor(factor, factor))
-                particle.data.vel.add(interaction)
-            }
-
-            let center = Victor(
+            )
+            let inX = mousePos.x > 0 && mousePos.x < app.renderer.width
+            let inY = mousePos.y > 0 && mousePos.y < app.renderer.height
+            
+            
+            particles.map((particle) => {     
+                
+                if (inX && inY) {
+                    let interaction = particle.data.pos.clone()
+                    .subtract(mousePos)
+                    let factor = 0.1/interaction.lengthSq()
+                    interaction.multiply(Victor(factor, factor))
+                    particle.data.vel.add(interaction)
+                    particle.data.vel.add(Victor(-100*interaction.y, 100*interaction.x))
+                }
+                
+                let center = Victor(
                 app.renderer.width/2,
                 app.renderer.height/2
-            )
-            let relPos = particle.data.pos.clone().subtract(center)
-            let acc = Victor(
-                0,
-                0
-            ).add(Victor(
-                0.1*relPos.x,
-                0.1*relPos.y
-            ))
-            .add(Victor(
-                600*Math.sin(elapsed/100)*Math.sin((relPos.x)/100),
-                0,
-            ))
-            .multiply(Victor(0.001, 0.001))
+                )
+                let relPos = particle.data.pos.clone().subtract(center)
+                // let acc = Victor(
+                //     0,
+                //     0
+                // ).add(Victor(
+                //     0.1*relPos.x,
+                //     0.1*relPos.y
+                // ))
+                // .multiply(Victor(0.001, 0.001))
+                
+                // particle.data.vel.add(acc)
+                particle.data.vel.multiply(Victor(0.99, 0.99))
+                particle.data.pos.add(particle.data.vel)
+                
+                if (particle.data.pos.x < 0) {
+                    particle.data.pos.x = app.renderer.width
+                } else if (particle.data.pos.x > app.renderer.width) {
+                    particle.data.pos.x = 0
+                }
+                
+                if (particle.data.pos.y < 0) {
+                    particle.data.pos.y = app.renderer.height
+                } else if (particle.data.pos.y > app.renderer.height) {
+                    particle.data.pos.y = 0
+                }
+                
+                
+                
+                
+                
+                particle.x = particle.data.pos.x
+                particle.y = particle.data.pos.y
+                
+                
+            })
             
-            particle.data.vel = acc
-            
-            particle.data.vel.add(acc)
-
-
-
-            // particle.data.vel.multiply(Victor(0.85, 0.85))
-            particle.data.pos.add(particle.data.vel)
-            
-            
-            particle.x = particle.data.pos.x
-            particle.y = particle.data.pos.y
-
-          
-        })
-
+        });
+        
+        function resize() {
+            if (window.innerWidth / window.innerHeight >= ratio) {
+                var w = window.innerHeight * ratio;
+                var h = window.innerHeight;
+            } else {
+                var w = window.innerWidth;
+                var h = window.innerWidth / ratio;
+            }
+            renderer.view.style.width = w + 'px';
+            renderer.view.style.height = h + 'px';
+        }
+        window.onresize = resize;
+        
     });
-
-
+    
+    
 </script>
 
-<div bind:this={wrapper}>
-
+<div id="wrapper" bind:this={wrapper}>
+    <h1>SETH LUPO</h1>
 </div>
+
+<style>
+
+
+
+    #wrapper {
+        width: 100vw;
+        height: 61vh;
+        overflow-x: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    h1 {
+        font-family: 'Martian Mono', monospace;
+        font-size: 12rem;
+        z-index: 10;
+        color: white; 
+        
+    }
+
+
+
+</style>
 
